42f55c24fa70f41025cdca17376ba0fba70567a1
Ryan Smith <main.ryansmith@gmail.com>
2012-12-19 20:16:10 +0000

    Adding directories.
    Library directory added.

---
+++ Library/core.js
@@ -0,0 +1,112 @@
+  var d = data;
+  d.innings = d.innings || [];
+  d.overs = d.overs || [];
+  d.balls = d.balls || [];
+  d.wickets = d.wickets || [];
+
+  // Duck Typing functions.
+  function testInt(value) {
+    // src: stackoverflow/questions/4514602/type-checking-in-javascript @Quentin.
+    return ((typeof value === "number") && (Math.floor(value) === value)) ? value || 0 : 0;
+  }
+
+  function testBool(value) {
+    return ((value === true) || (value === false)) ? value || false : false;
+  }
+
+  function testDate(value) {
+    // src: stackoverflow/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript @Borgar.
+    return (Object.prototype.toString.call(d) === "[object Date]") ? (isNaN(d.getTime()) ? null : value) : null;
+  }
+
+  // Data manipulation.
+  function pushInning(battingTeam) {
+    d.innings.push({
+      "battingTeam" : battingTeam
+    });
+    return true;
+  }
+
+  function pushOver(bowlerId) {
+    d.overs.push({
+      "inningId" : d.innings.length - 1,
+      "bowlerId" : bowlerId
+    });
+    return true;
+  }
+
+  function pushBall(batsmanId, runs, wideBall, noBall, byes, legByes, wicket, wagonX, wagonY, pitchX, pitchY, dateTime) {
+    // Duck Typing.
+    runs = testInt(runs);
+    wagonX = testInt(wagonX);
+    wagonY = testInt(wagonY);
+    pitchX = testInt(pitchX);
+    pitchY = testInt(pitchY);
+
+    wicket = testBool(wicket);
+    wideBall = testBool(wideBall);
+    noBall = testBool(noBall);
+    byes = testBool(byes);
+    legByes = testBool(legByes);
+
+    dateTime = testDate(dateTime);
+
+    // Functionality.
+    d.balls.push({
+      "inningId" : d.innings.length - 1,
+      "overId" : d.overs.length - 1,
+      "batsmanId" : batsmanId,
+      "runs" : runs,
+      "wideBall" : wideBall,
+      "noBall" : noBall,
+      "byes" : byes,
+      "legByes" : legByes,
+      "wicketId" : wicket === true ? d.wickets.length - 1 : -1,
+      "wagonX" : wagonX,
+      "wagonY" : wagonY,
+      "pitchX" : pitchX,
+      "pitchY" : pitchY,
+      "dateTime" : dateTime
+    });
+    return true;
+  }
+
+  function pushWicket(batsmanId, fielderId, howOut) {
+    // Ten ways to get out http://news.bbc.co.uk/sport1/hi/cricket/rules_and_equipment/4180344.stm
+    // Duck check.
+    var valid = (new RegExp("^(bowled|lbw|handled the ball|hit wicket|hit the ball twice)$", "i")).test(howOut) ? "bowler" : 
+      (new RegExp("^(caught|stumped)$", "i")).test(howOut) ? "both" : 
+      (new RegExp("^(runout)$", "i")).test(howOut) ? "fielder" : 
+      (new RegExp("^(obstruction|timed out)$", "i")).test(howOut) ? "none" : false,
+      bowlerId = valid === "bowler" || valid === "both" ? d.overs[d.overs.length - 1].bowlerId : -1,
+      fielderId = valid === "fielder" || valid === "both" ? fielderId : -1;
+
+    // Functionality.
+    if (valid !== false) {
+      d.wickets.push({
+        "batsmanId": batsmanId,
+        "bowlerId": bowlerId,
+        "fielderId": fielderId,
+        "howOut": howOut
+      });
+    }
+
+    return valid !== false;
+  }
+
+  function pop() {
+    var list = !(d.innings.length === d.overs[d.overs.length - 1].inningId + 1) ? d.innings : !(d.overs.length === d.balls[d.balls.length - 1].overId + 1) ? d.overs : !(d.balls[d.balls.length - 1].wicketId !== 0) ? d.balls : d.wickets;
+
+    list.pop();
+    return true;
+  }
+
+  // Shortcut to current scorebook.
+  return {
+    "addInnings": pushInning,
+    "addOver": pushOver,
+    "addBall": pushBall,
+    "addWicket": pushWicket,
+    "undo": pop,
+    "data": d
+  };
---
+++ Library/intro.js
@@ -0,0 +1,10 @@
+/*!
+ * Scorebook Library v@VERSION
+ *
+ * Copyright (C) 2012 Ryan Smith
+ * All rights reserved.
+ *
+ * Date: @DATE
+ */
+var scorebook = function(data, undefined) {
+  "use strict";

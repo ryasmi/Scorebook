c0e58a681d00e2c6498aa94cfe90096875e5e120
Ryan Smith <main.ryansmith@gmail.com>
2012-12-26 17:58:06 +0000

    Changes to core.js
    Improved commenting and pull functionality.

--- Library/core.js
+++ Library/core.js
@@ -1,4 +1,6 @@
   var d = data;
+
+  // Check if any data is not defined and define it as an empty list.
   d.innings = d.innings || [];
   d.overs = d.overs || [];
   d.balls = d.balls || [];
@@ -22,16 +24,20 @@
   // Data manipulation.
   function pushInning(battingTeam) {
     d.innings.push({
+      "id" : d.innings.length,
       "battingTeam" : battingTeam
     });
+
     return true;
   }
 
   function pushOver(bowlerId) {
     d.overs.push({
+      "id" : d.overs.length,
       "inningId" : d.innings.length - 1,
       "bowlerId" : bowlerId
     });
+
     return true;
   }
 
@@ -51,9 +57,9 @@
 
     dateTime = testDate(dateTime);
 
-    // Functionality.
+    // Create a new ball.
     d.balls.push({
-      "inningId" : d.innings.length - 1,
+      "id" : d.balls.length,
       "overId" : d.overs.length - 1,
       "batsmanId" : batsmanId,
       "runs" : runs,
@@ -61,13 +67,14 @@
       "noBall" : noBall,
       "byes" : byes,
       "legByes" : legByes,
-      "wicketId" : wicket === true ? d.wickets.length - 1 : -1,
+      "wicketId" : wicket === true ? d.wickets.length - 1 : null,
       "wagonX" : wagonX,
       "wagonY" : wagonY,
       "pitchX" : pitchX,
       "pitchY" : pitchY,
       "dateTime" : dateTime
     });
+
     return true;
   }
 
@@ -78,12 +85,13 @@
           (new RegExp("^(caught|stumped)$", "i")).test(howOut) ? "both" :
           (new RegExp("^(runout)$", "i")).test(howOut) ? "fielder" :
           (new RegExp("^(obstruction|timed out)$", "i")).test(howOut) ? "none" : false,
-      bowlerId = valid === "bowler" || valid === "both" ? d.overs[d.overs.length - 1].bowlerId : -1;
-    if (valid !== "fielder" && valid !== "both") {fielderId = -1;}
+      bowlerId = ((valid === "bowler") || (valid === "both")) ? d.overs[d.overs.length - 1].bowlerId || null : null;
+      fielderId = ((valid === "fielder") || (valid === "both")) ? fielderId || null : null;
 
-    // Functionality.
+    // Create a new wicket if duck checks were successful.
     if (valid !== false) {
       d.wickets.push({
+        "id" : d.wickets.length,
         "batsmanId": batsmanId,
         "bowlerId": bowlerId,
         "fielderId": fielderId,
@@ -91,23 +99,33 @@
       });
     }
 
+    // Return true if a new wicket was added.
     return valid !== false;
   }
 
   function pop() {
+    // Determine which list should be popped.
     var list = d.innings.length !== (d.overs[d.overs.length - 1].inningId + 1) ? d.innings :
           d.overs.length !== d.balls[d.balls.length - 1].overId + 1 ? d.overs :
-          d.balls[d.balls.length - 1].wicketId === -1 ? d.balls : d.wickets;
+          d.balls[d.balls.length - 1].wicketId === null ? d.balls : d.wickets;
 
+    // Remove the wicket id from the ball because the wicket will no longer exist.
+    if (list === d.wickets) {
+      d.balls[d.balls.length - 1].wicketId = null;
+    }
+
+    // Pop an item of the selected list and return true to show that the undo was successful.
     list.pop();
     return true;
   }
 
   function pullInning(inningId, battingTeam) {
+    // Create a return list and then two functions for instance comparison.
     var returnInnings = [],
-      isInning = inningId ? function () {return i === inningId;} : true,
-      isBattingTeam = battingTeam ? function () {return innings[i].battingTeam === battingTeam;} : true;
+      isInning = inningId ? function () {return i === inningId;} : function () {return true;},
+      isBattingTeam = battingTeam ? function () {return innings[i].battingTeam === battingTeam;} : function () {return true;};
 
+    // Attempt to match each innings with the input.
     for (i = 0; i < innings.length; i += 1) {
       if (isBattingTeam() && isInning()) {
         returnInnings.push(innings[i]);
@@ -117,6 +135,18 @@
     return returnInnings;
   }
 
+  function pullOver(inningId, overId, bowlerId) {
+    return true;
+  }
+
+  function pullBall() {
+    return true;
+  }
+
+  function pullWicket() {
+    return true;
+  }
+
   // Public functions and variables.
   return {
     "addInnings": pushInning,
@@ -124,9 +154,12 @@
     "addBall": pushBall,
     "addWicket": pushWicket,
     "undo": pop,
+    "getInnings" : pullInning,
+    //"getOver" : pullOver,
+    //"getBall" : pullBall,
+    //"getWicket" : pullWicket,
     "data": d
   };
 
-  // TODO: Better comments.
   // TODO: Add pull functions.
-  // Commit test.
+  // TODO: Optimisiation. Add some heuristics to pull functions (particularly for id). If an inningsId is given in pullInning it is obvious that only inning can be returned from the list.

2646c6f2246af5964f139d6129e9e9efc38cff87
Ryan Smith <main.ryansmith@gmail.com>
2012-12-30 15:58:27 +0000

    Auto version incrementation
    Now auto increments the version number after each build.

--- grunt.js
+++ grunt.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
     min: {
       "release": {
@@ -18,15 +19,38 @@ module.exports = function(grunt) {
     }
   });
 
+  // Incrementing the version.
+  grunt.registerTask("incrementVersion", function () {
+    incrementVersion();
+  });
+
+  function incrementVersion() {
+    // Get the info file.
+    var infoFile = "lib/info.json",
+      info = grunt.file.readJSON(infoFile);
+
+    // Increment version.
+    info.version.build = info.version.build > 49 ? 0 : info.version.build + 1;
+    info.version.minor = info.version.build === 0 ? (info.version.minor > 8 ? 0 : info.version.minor + 1) : info.version.minor;
+    info.version.major = info.version.minor === 0 ? info.version.major + 1 : info.version.major;
+    grunt.file.write(infoFile, JSON.stringify(info));
+
+    // major.minor.build
+    return [
+      info.version.major,
+      info.version.minor,
+      info.version.build
+    ].join(".");
+  }
+
   // Adds date and version.
   grunt.registerTask("date-version", function () {
     // src: github/jQuery/jQuery/Gruntfile.js @jQuery.
-    info = grunt.file.readJSON("lib/info.json");
     compiled = grunt.file.read("lib/intro.js");
-    compiled = compiled.replace(/@VERSION/g, info.version).replace("@DATE", function () {
+    compiled = compiled.replace(/@VERSION/g, incrementVersion()).replace("@DATE", function () {
       var date = new Date();
 
-      // YYYY-MM-DD
+      // YYYY.MM.DD
       return [
         date.getFullYear(),
         date.getMonth() + 1,
@@ -34,13 +58,13 @@ module.exports = function(grunt) {
       ].join(".");
     });
 
-    // Write concatenated source to file
+    // Write concatenated source to file.
     grunt.file.write("src/copyright.js", compiled);
   });
 
-  // Default grunt
+  // Default grunt.
   grunt.registerTask("default", ["min:release", "date-version", "concat:release"]);
 
-  // Short list as a high frequency watch task
-  grunt.registerTask("dev", ["min:test"]);
+  // Short list as a high frequency watch task.
+  grunt.registerTask("dev", ["min:test", "incrementVersion"]);
 };
--- src/copyright.js
+++ src/copyright.js
@@ -1,8 +1,8 @@
 /*!
- * Scorebook Library v0.1
+ * Scorebook Library v0.1.7.
  *
- * Copyright (C) 2012 Ryan Smith
+ * Copyright (C) 2012 Ryan Smith.
  * All rights reserved.
  *
- * Date: 2012-12-30
+ * Date: 2012.12.30.
  */

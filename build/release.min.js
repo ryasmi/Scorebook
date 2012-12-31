77fdab0ea95b25ca4796a8018e9aed32aed6f6d8
Ryan Smith <main.ryansmith@gmail.com>
2012-12-31 12:19:18 +0000

    Updated licence.

--- build/release.min.js
+++ build/release.min.js
@@ -1,8 +1,9 @@
 /*!
- * Scorebook Library v0.2.7.
+ * Scorebook Library v0.2.8.
  *
  * Copyright (C) 2012 Ryan Smith.
- * All rights reserved.
+ * This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License (CC BY-NC-SA 3.0).
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
  *
  * Date: 2012.12.31.
  */
--- src/intro.js
+++ src/intro.js
@@ -2,7 +2,8 @@
  * Scorebook Library v@VERSION.
  *
  * Copyright (C) 2012 @AUTHOR.
- * All rights reserved.
+ * This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License (CC BY-NC-SA 3.0).
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
  *
  * Date: @DATE.
  */

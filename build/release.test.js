6f81d4d549f0497f8f5456304e411785d51d4ed9
Ryan Smith <0ryansmith1994@gmail.com>
2013-06-11 20:13:21 +0100

    $1

--- build/release.test.js
+++ build/release.test.js
@@ -3,8 +3,8 @@
  * https://www.github.com/ryansmith94/Scorebook
  *
  * Copyright 2013, Ryan Smith and contributors.
- * Released under the CC BY-NC-SA 3.0 license.
- * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * Released under the MIT license.
+ * http://opensource.org/licenses/MIT
  *
- * 2013-06-11, 14:53.
+ * 2013-06-11, 20:13.
 */

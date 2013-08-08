30fb92fcc2c98599220c01a9554f57ce3a8e0d5f
Ryan Smith <0ryansmith1994@gmail.com>
2013-08-08 18:28:29 +0100

    Removed version control from Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,10 @@
 /* jshint node: true */
 module.exports = function (grunt) {
     'use strict';
-    var pkg = grunt.file.readJSON('package.json');
 
     // Project configuration.
     grunt.initConfig({
-        pkg: pkg,
+        pkg: grunt.file.readJSON('package.json'),
         uglify: {
             options: {
                 banner: grunt.file.read('src/banner.txt') + '\n'
@@ -13,10 +12,6 @@ module.exports = function (grunt) {
             build: {
                 src: 'build/release.min.js',
                 dest: 'build/release.min.js'
-            },
-            test: {
-                src: 'build/release.test.js',
-                dest: 'build/release.test.js'
             }
         },
         concat: {
@@ -26,10 +21,6 @@ module.exports = function (grunt) {
             dist: {
                 src: ['src/**/*.js'],
                 dest: 'build/release.min.js'
-            },
-            test: {
-                src: ['test/**/*.js'],
-                dest: 'build/release.test.js'
             }
         },
         jshint: {
@@ -45,32 +36,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    // Custom task for running test files.
-    grunt.registerTask('test', function () {
-        require('./build/release.test.js');
-    });
-
-    var setVersion = function (fn) {
-        pkg.version = fn(pkg.version.split('.'));
-        grunt.file.write('package.json', JSON.stringify(pkg, null, 4));
-        grunt.task.run(['jshint', 'concat', 'uglify', 'test']);
-    };
-
-    grunt.registerTask('default', function () {
-        setVersion(function (version) {
-            return version[0] + '.' + version[1] + '.' + (Number(version[2]) + 1);
-        });
-    });
-
-    grunt.registerTask('minor', function () {
-        setVersion(function (version) {
-            return version[0] + '.' + (Number(version[1]) + 1) + '.0';
-        });
-    });
-
-    grunt.registerTask('major', function () {
-        setVersion(function (version) {
-            return (Number(version[0]) + 1) + '.0.0';
-        });
-    });
+    // Tasks.
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };

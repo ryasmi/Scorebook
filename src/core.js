3f2278ce65dc27acd7ab4c51bd445a91782b28fe
Ryan Smith <0ryansmith1994@gmail.com>
2014-07-30 12:23:17 +0100

    Makes code "DRY" and easier to maintain.

--- src/core.js
+++ src/core.js
@@ -2,123 +2,116 @@
 (function (scope, Number, Boolean) {
     'use strict';
 
-    var resembles = function (needle, haystack) {
-        var keys = Object.keys(needle);
+    var slice = function (args) {
+        return Array.prototype.slice.call(args, 0);
+    };
+
+    var resembles = function (haystack) {
+        var keys = Object.keys(haystack);
         var len = keys.length;
-        var key = 0;
 
-        return (function check() {
-            if (needle[keys[key]] === haystack[keys[key]]) {
+        return function (needle) {
+            var key = 0;
+
+            while (key <= len && needle[keys[key]] === haystack[keys[key]]) {
                 key += 1;
-                return (key <= len) ? check() : true;
             }
-            return false;
-        }());
+
+            return key > len;
+        };
     };
 
-    var appendTo = function (parentList, sbList, obj) {
-        var parent = parentList[parentList.length - 1];
-        obj.id = sbList.length;
-        obj.parent = parent;
-        obj.children = [];
-        parent.children.push(obj);
-        sbList.push(obj);
+    var appendTo = function (retObj, parentList, sbList, parse) {
+        parse = parse || function (obj) {return obj; };
+
+        return function () {
+            var parent = parentList[parentList.length - 1];
+            var obj = parse.apply({}, slice(arguments));
+            obj.id = sbList.length;
+            obj.parent = parent;
+            obj.children = [];
+            parent.children.push(obj);
+            sbList.push(obj);
+            return retObj;
+        };
     };
 
-    var getFrom = function (list, obj) {
-        return list.filter(function (item) {
-            return resembles(obj, item);
-        });
+    var getFrom = function (list) {
+        return function (obj) {
+            return list.filter(resembles(obj));
+        };
     };
 
     var nanToNumber = function (value) {
-        return isNaN(Number(value)) ? 0 : value;
+        var value = Number(value);
+        return isNaN(value) ? 0 : value;
     };
 
     var Scorebook = function () {
         var self = this;
-        var innings = [];
-        var overs = [];
-        var balls = [];
-        var wickets = [];
 
-        self.addInnings = function (battingTeam) {
-            appendTo([{children: []}], innings, {
+        self.innings = [];
+        self.overs = [];
+        self.balls = [];
+        self.wickets = [];
+
+        self.addInnings = appendTo(self, [{children: []}], self.innings, function (battingTeam) {
+            return {
                 'battingTeam': battingTeam
-            });
-            return self;
-        };
+            };
+        });
 
-        self.addOver = function (bowler) {
-            appendTo(innings, overs, {
+        self.addOver = appendTo(self, self.innings, self.overs, function (bowler) {
+            return {
                 'bowler': bowler
-            });
-            return self;
-        };
+            };
+        });
 
-        self.addBall = function (batsman, runs, wideBall, noBall, byes, legByes, wagonX, wagonY, pitchX, pitchY, batPen, bwlPen) {
-            appendTo(overs, balls, {
-                'batsman': batsman,
+        self.addBall = appendTo(self, self.overs, self.balls, function (runs, batsman, opts) {
+            opts = opts || {};
+
+            return {
+                'batsman': batsman || self.balls[self.balls.length - 1].batsman,
                 'runs': nanToNumber(runs),
-                'wideBall': Boolean(wideBall),
-                'noBall': Boolean(noBall),
-                'byes': Boolean(byes),
-                'legByes': Boolean(legByes),
-                'wagonX': Number(wagonX),
-                'wagonY': Number(wagonY),
-                'pitchX': Number(pitchX),
-                'pitchY': Number(pitchY),
-                'batPen': nanToNumber(batPen),
-                'bwlPen': nanToNumber(bwlPen),
+                'wideBall': Boolean(opts.wideBall),
+                'noBall': Boolean(opts.noBall),
+                'byes': Boolean(opts.byes),
+                'legByes': Boolean(opts.legByes),
+                'wagonX': nanToNumber(opts.wagonX),
+                'wagonY': nanToNumber(opts.wagonY),
+                'pitchX': nanToNumber(opts.pitchX),
+                'pitchY': nanToNumber(opts.pitchY),
+                'batPen': nanToNumber(opts.batPen),
+                'bwlPen': nanToNumber(opts.bwlPen),
                 'dateTime': new Date()
-            });
-            return self;
-        };
+            };
+        });
 
-        self.addWicket = function (batsman, howOut, fielder) {
-            appendTo(balls, wickets, {
-                'batsman': batsman,
+        self.addWicket = appendTo(self, self.balls, self.wickets, function (howOut, batsman, fielder) {
+            return {
+                'batsman': batsman || self.balls[self.balls.length - 1].batsman,
                 'howOut': howOut,
                 'fielder': fielder
-            });
-            return self;
-        };
+            };
+        });
 
-        self.getInnings = function (inning) {
-            return getFrom(innings, inning);
-        };
-
-        self.getOvers = function (over) {
-            return getFrom(overs, over);
-        };
-
-        self.getBalls = function (ball) {
-            return getFrom(balls, ball);
-        };
-
-        self.getWickets = function (wicket) {
-            return getFrom(wickets, wicket);
-        };
+        self.getInnings = getFrom(self.innings);
+        self.getOvers = getFrom(self.overs);
+        self.getBalls = getFrom(self.balls);
+        self.getWickets = getFrom(self.wickets);
 
         self.undo = function () {
             // Determine which list should be popped.
-            var lastWicket = wickets[wickets.length - 1] || {parent: null};
-            var lastBall = balls[balls.length - 1] || {parent: null};
-            var lastOver = overs[overs.length - 1] || {parent: null};
-            var lastInning = innings[innings.length - 1] || {parent: null};
-            var list = lastWicket.parent === lastBall ? [wickets, lastBall] : (lastBall.parent === lastOver ? [balls, lastOver] : (lastOver.parent === lastInning ? [overs, lastInning] : [innings, {children: [lastInning]}]));
+            var lastWicket = self.wickets[self.wickets.length - 1] || {parent: null};
+            var lastBall = self.balls[self.balls.length - 1] || {parent: null};
+            var lastOver = self.overs[self.overs.length - 1] || {parent: null};
+            var lastInning = self.innings[self.innings.length - 1] || {parent: null};
+            var list = lastWicket.parent === lastBall ? [self.wickets, lastBall] : (lastBall.parent === lastOver ? [self.balls, lastOver] : (lastOver.parent === lastInning ? [self.overs, lastInning] : [self.innings, {children: [lastInning]}]));
 
             // Pop an item off the selected list.
             list[1].children.pop();
             list[0].pop();
             return self;
-        };
-
-        self.data = {
-            'innings': innings,
-            'overs': overs,
-            'balls': balls,
-            'wickets': wickets
         };
 
         return self;
